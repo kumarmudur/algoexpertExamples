6a8fd2b6839ef6b75c40f0986c74847d110a18c3
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-07-12 06:50:42 +0530

    [Strings] palindrome check example solution3

--- strings/palindromeCheck.js
+++ strings/palindromeCheck.js
@@ -20,5 +20,17 @@ function isPalindrome(string) {
     return string === reversedChars.join();
 }
 
+// solution 3
+// O(n) time | O(1) space
+function isPalindrome(string) {
+    let leftIdx = 0;
+    let rightIdx = string.length - 1;
+    while (leftIdx < rightIdx) {
+        if (string[leftIdx] !== string[rightIdx]) return false;
+        leftIdx++;
+        rightIdx--;
+    }
+    return true;
+}
 
 
