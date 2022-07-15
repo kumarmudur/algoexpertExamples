118dc5aabdfd6219558f93f0fa90313a1db1c44d
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-07-15 07:16:01 +0530

    [Recursion] nth fib solution 3

--- recursion/nthFib.js
+++ recursion/nthFib.js
@@ -7,3 +7,24 @@ const getNthFib = (n) => {
     else if (n === 1) return 0;
     else return getNthFib(n - 1) + getNthFib(n - 2);
 }
+
+// Solution 2
+// O(n) time  | O(n) space
+const getNthFib = (n, memoize = {1: 0, 2: 1}) => {
+    if (n in memoize) return memoize[n];
+    else return memoize[n] = getNthFib(n - 1, memoize); + getNthFib(n - 2, memoize);
+}
+
+// Solution 3
+// O(n) time | O(1) space
+const getNthFib = (n) => {
+    const lastTwo = [0, 1];
+    let counter = 3;
+    while (counter <= n) {
+        const nextFib = lastTwo[0] + lastTwo[1];
+        lastTwo[0] = lastTwo[1];
+        lastTwo[1] = nextFib;
+        counter++;
+    }
+    return n > 1 ? lastTwo[1] : lastTwo[0];
+}
