09ff5ca1eee134775f074ddf7e0dce99a3f74849
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-07-15 07:49:44 +0530

    [Stacks] balanced brackets example

--- recursion/nthFib.js
+++ recursion/nthFib.js
@@ -2,7 +2,7 @@
 
 // Solution 1
 // O(n^2) time | O(n) space
-const getNthFib = (n) => {
+const getNthFib1 = (n) => {
     if (n === 2) return 1;
     else if (n === 1) return 0;
     else return getNthFib(n - 1) + getNthFib(n - 2);
@@ -10,14 +10,14 @@ const getNthFib = (n) => {
 
 // Solution 2
 // O(n) time  | O(n) space
-const getNthFib = (n, memoize = {1: 0, 2: 1}) => {
+const getNthFib2 = (n, memoize = {1: 0, 2: 1}) => {
     if (n in memoize) return memoize[n];
     else return memoize[n] = getNthFib(n - 1, memoize); + getNthFib(n - 2, memoize);
 }
 
 // Solution 3
 // O(n) time | O(1) space
-const getNthFib = (n) => {
+const getNthFib3 = (n) => {
     const lastTwo = [0, 1];
     let counter = 3;
     while (counter <= n) {
