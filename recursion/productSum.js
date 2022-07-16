783a7abfe249d48de09b299ea1b836bf525f5bfd
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-07-16 19:01:43 +0530

    [Recursion] product sum example solution 2

--- recursion/productSum.js
+++ recursion/productSum.js
@@ -6,10 +6,20 @@ function productSum(array, multiplier = 1) {
     let sum = 0;
     for (const element of array) {
         if (Array.isArray(element)) {
-            sum += productSum(element, multiplier);
+            sum += productSum(element, multiplier + 1);
         } else {
             sum += element;
         }
     }
+    return sum * multiplier;
+}
+
+
+// Solution 2
+function productSum1(element, multiplier) {
+    const sum = array.reduce((a, e) => {
+        if (Array.isArray(e)) return a + productSum(e, multiplier + 1);
+        return a + e;
+    }, 0);
     return sum * multiplier;
 }
